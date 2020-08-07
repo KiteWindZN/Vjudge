8a92c459ac54986c0a7ca5f96fb85942bf9e9b67
zhangnan <15605189713@163.com>
2020-08-07 18:46:11 +0800

    12214

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+int n_11214;
+int m_11214;
+const int max_11214=12;
+int vis_11214[4][50];
+int graph_11214[max_11214][max_11214];
+int maxd_11214;
+
+bool isOK_11214(){
+    for(int i=0;i<n_11214;i++){
+        for(int j=0;j<m_11214;j++){
+            if(graph_11214[i][j]&&(vis_11214[0][i]==0||vis_11214[1][j]==0||vis_11214[2][i-j+max_11214]==0||vis_11214[3][i+j]==0)){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool dfs_11214(int i,int j,int cur){
+
+    if(cur==(maxd_11214+1)){
+        if(isOK_11214())
+            return true;
+        else return false;
+    }
+    for(;i<n_11214;i++){
+        for(;j<m_11214;j++){
+            int t0,t1,t2,t3;
+            t0=vis_11214[0][i];
+            t1=vis_11214[1][j];
+            t2=vis_11214[2][i-j+max_11214];
+            t3=vis_11214[3][i+j];
+
+            vis_11214[0][i]=1;
+            vis_11214[1][j]=1;
+            vis_11214[2][i-j+max_11214]=1;
+            vis_11214[3][i+j]=1;
+
+            if(dfs_11214(i,j+1,cur+1))
+                return;
+            vis_11214[0][i]=t0;
+            vis_11214[1][j]=t1;
+            vis_11214[2][i-j+max_11214]=t2;
+            vis_11214[3][i+j]=t3;
+        }
+    }
+    return false;
+}
+int main(){
+    int T;
+    cin>>T;
+    while(T){
+        cin>>n_11214>>m_11214;
+        for(int i=0;i<n_11214;i++){
+            string line;
+            cin>>line;
+            memset(graph_11214,0,sizeof(graph_11214));
+            for(int j=0;j<m_11214;j++){
+                char ch=line[j];
+                if(ch=='X'){
+                    graph_11214[i][j]=1;
+                }
+            }
+        }
+
+        for(maxd_11214=1;;maxd_11214++){
+            memset(vis_11214,0,sizeof(vis_11214));
+            if(dfs_11214(0,0,1))
+                break;
+        }
+        cout<<maxd_11214<<endl;
+        T--;
+    }
+    return 0;
+}
